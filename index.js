bc898f03ff05cbadafc2eedce4d16936ff557278
Gerald Arthur Moore <gerald.moore42@gmail.com>
2025-03-08 23:35:05 +0200

    heroku setup

--- index.js
+++ index.js
@@ -12,19 +12,22 @@ app.use('/api', routes);
 
 const mongoString = process.env.DATABASE_URL;
 
-mongoose.connect(mongoString);
+mongoose.connect(mongoString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 const database = mongoose.connection;
 
 database.on('error', (error) => {
-    console.log(error)
-})
+    console.log('Database Connection Error:', error);
+});
 
 database.once('connected', () => {
-    console.group('Database Connected');
-})
-
+    console.log('Database Connected');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
-    console.log('Listening on Port ' + port);
-})
+    console.log(`Listening on Port ${port}`);
+});
