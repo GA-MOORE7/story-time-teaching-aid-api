7daf6fa7e3198bd8fdfabf9e58b5c2b3134573c0
Gerald Arthur Moore <gerald.moore42@gmail.com>
2025-03-08 23:27:56 +0200

    heroku setup

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ database.once('connected', () => {
     console.group('Database Connected');
 })
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`);
+
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+    console.log('Listening on Port ' + port);
 })
